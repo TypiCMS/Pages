87771a86854fbe3768949008f3862614f6ec0c89
Samuel De Backer <sdebacker@gmail.com>
2015-04-27 15:58:10 +0200

    templates in select / options

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -63,7 +63,7 @@
         @include('core::admin._image-fieldset', ['field' => 'image'])
         @include('core::admin._galleries-fieldset')
         {!! BootForm::select(trans('validation.attributes.module'), 'module', TypiCMS::getModulesForSelect(), null, array('class' => 'form-control')) !!}
-        {!! BootForm::text(trans('validation.attributes.template'), 'template') !!}
+        {!! BootForm::select(trans('validation.attributes.template'), 'template', TypiCMS::getPageTemplates(), null, array('class' => 'form-control')) !!}
         @if (! $model->id)
         {!! BootForm::select(trans('validation.attributes.add_to_menu'), 'add_to_menu', ['' => ''] + Menus::all()->lists('title', 'id'), null, array('class' => 'form-control')) !!}
         @endif
