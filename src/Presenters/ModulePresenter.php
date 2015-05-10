b7711c725996b25ff1da65d6c0dd86681a81697c
Samuel De Backer <sdebacker@gmail.com>
2015-05-10 15:57:24 +0200

    abstract presenter

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -1,7 +1,7 @@
 <?php
 namespace TypiCMS\Modules\Pages\Presenters;
 
-use Laracasts\Presenter\Presenter;
+use TypiCMS\Modules\Core\Presenters\Presenter;
 
 class ModulePresenter extends Presenter
 {
