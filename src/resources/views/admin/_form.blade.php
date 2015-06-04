acba914f47caaeb698b86132d5e668708f482a6e
Samuel De Backer <sdebacker@gmail.com>
2015-06-04 22:38:04 +0200

    template() method name

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -58,7 +58,7 @@
         @include('core::admin._image-fieldset', ['field' => 'image'])
         @include('core::admin._galleries-fieldset')
         {!! BootForm::select(trans('validation.attributes.module'), 'module', TypiCMS::getModulesForSelect(), null, array('class' => 'form-control')) !!}
-        {!! BootForm::select(trans('validation.attributes.template'), 'template', TypiCMS::getPageTemplates(), null, array('class' => 'form-control')) !!}
+        {!! BootForm::select(trans('validation.attributes.template'), 'template', TypiCMS::templates(), null, array('class' => 'form-control')) !!}
         @if (! $model->id)
         {!! BootForm::select(trans('validation.attributes.add_to_menu'), 'add_to_menu', ['' => ''] + Menus::all()->lists('title', 'id')->all(), null, array('class' => 'form-control')) !!}
         @endif
