3bed9ab1eae1131fc919c4d0d55d92da691f0ea8
Samuel De Backer <sdebacker@gmail.com>
2016-01-04 23:17:03 +0100

    Menulink has moved

--- src/Observers/AddToMenuObserver.php
+++ src/Observers/AddToMenuObserver.php
@@ -3,7 +3,7 @@
 namespace TypiCMS\Modules\Pages\Observers;
 
 use Illuminate\Support\Facades\Request;
-use TypiCMS\Modules\Menulinks\Models\Menulink;
+use TypiCMS\Modules\Menus\Models\Menulink;
 use TypiCMS\Modules\Pages\Models\Page;
 
 class AddToMenuObserver
@@ -27,7 +27,7 @@ class AddToMenuObserver
             foreach ($model->translations as $translation) {
                 $data[$translation->locale]['title'] = $translation->title;
             }
-            app('TypiCMS\Modules\Menulinks\Repositories\MenulinkInterface')->create($data);
+            app('TypiCMS\Modules\Menus\Repositories\MenulinkInterface')->create($data);
         }
     }
 
