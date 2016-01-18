1e7de66f1d27bf199e6bf57b80ed762c0b8fe9b2
Samuel De Backer <sdebacker@gmail.com>
2016-01-18 21:46:33 +0100

    migration style

--- src/database/migrations/2013_08_29_174626_create_pages_table.php
+++ src/database/migrations/2013_08_29_174626_create_pages_table.php
@@ -14,58 +14,41 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
-
             $table->string('meta_robots_no_index')->default(0);
             $table->string('meta_robots_no_follow')->default(0);
-
             $table->string('image')->nullable();
             $table->integer('position')->unsigned()->default(0);
             $table->integer('parent_id')->unsigned()->nullable()->default(null);
-
             $table->boolean('private')->default(0);
             $table->boolean('is_home')->default(0);
             $table->boolean('redirect')->default(0);
             $table->boolean('no_cache')->default(0);
-
             $table->text('css')->nullable();
             $table->text('js')->nullable();
-
             $table->string('module')->nullable();
             $table->string('template')->nullable();
-
             $table->timestamps();
-
             $table->foreign('parent_id')->references('id')->on('pages')->onDelete('cascade');
         });
 
         Schema::create('page_translations', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->integer('page_id')->unsigned();
-
             $table->string('locale')->index();
-
             $table->string('slug')->nullable();
             $table->string('uri')->nullable();
-
             $table->string('title');
             $table->text('body');
-
             $table->boolean('status')->default(0);
-
             $table->string('meta_title')->nullable();
             $table->string('meta_keywords')->nullable();
             $table->string('meta_description')->nullable();
-
             $table->timestamps();
-
             $table->unique(['page_id', 'locale']);
             $table->unique(['locale', 'uri']);
             $table->foreign('page_id')->references('id')->on('pages')->onDelete('cascade');
-
         });
     }
 
