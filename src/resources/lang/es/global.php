ce8a6b3aed6143cd005da8fc705cbaa74f3929b0
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 11:57:38 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name'  => 'Páginas',
+    'pages' => 'pagina|paginas',
+    'New'   => 'Nueva página',
+    'Edit'  => 'Editar página',
+    'Back'  => 'Volver a las páginas',
+];
