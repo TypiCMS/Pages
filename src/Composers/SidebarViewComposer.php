5dd38662990f77e535004bf69f8f73bf6373b3f9
Samuel De Backer <sdebacker@gmail.com>
2016-04-04 19:01:09 +0200

    Gate::allow in sidebar

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -3,6 +3,7 @@
 namespace TypiCMS\Modules\Pages\Composers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Gate;
 use Maatwebsite\Sidebar\SidebarGroup;
 use Maatwebsite\Sidebar\SidebarItem;
 
@@ -19,7 +20,7 @@ class SidebarViewComposer
                 $item->route('admin::index-pages');
                 $item->append('admin::create-pages');
                 $item->authorize(
-                    auth()->user()->can('index-pages')
+                    Gate::allows('index-pages')
                 );
             });
         });
