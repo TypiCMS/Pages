533452386f6cebccc6406d69185e464a13a3be3e
Nasko <nasko@webfactory.bg>
2016-04-06 13:12:13 +0300

    admintool4 skin

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -13,7 +13,7 @@ class SidebarViewComposer extends BaseSidebarViewComposer
     {
         $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
             $group->id = 'content';
-            $group->weight = 30;
+            $group->weight = 1;
             $group->addItem(trans('pages::global.name'), function (SidebarItem $item) {
                 $item->icon = config('typicms.pages.sidebar.icon', 'icon fa fa-fw fa-file');
                 $item->weight = config('typicms.pages.sidebar.weight');
