79afb70150f635503686a826772ff11bd84b8ffc
Samuel De Backer <sdebacker@gmail.com>
2016-04-08 16:15:34 +0200

    Better translation

--- src/resources/lang/es/global.php
+++ src/resources/lang/es/global.php
@@ -6,5 +6,5 @@ return [
     'New'                                               => 'Nueva página',
     'Edit'                                              => 'Editar página',
     'Back'                                              => 'Volver a las páginas',
-    'A page with children cannot be linked to a module' => 'Una página con niños no puede estar relacionada con un módulo.',
+    'A page with children cannot be linked to a module' => 'Una página con descendiente no puede estar vinculada a un módulo.',
 ];
