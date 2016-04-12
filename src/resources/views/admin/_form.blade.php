4714b8df29f04911616a9260d4956f226a476d05
Samuel De Backer <sdebacker@gmail.com>
2016-04-12 22:02:29 +0200

    form.js has splited

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -1,6 +1,5 @@
 @section('js')
     <script src="{{ asset('components/ckeditor/ckeditor.js') }}"></script>
-    <script src="{{ asset('js/admin/form.js') }}"></script>
 @endsection
 
 @include('core::admin._buttons-form')
