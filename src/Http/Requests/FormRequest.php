c680123282a0c6ab6607554d72e17edee42b9e8d
Samuel De Backer <sdebacker@gmail.com>
2017-01-23 22:19:39 +0100

    nullable validation rules

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -9,12 +9,12 @@ class FormRequest extends AbstractFormRequest
     public function rules()
     {
         return [
-            'template' => 'alpha_dash|max:255',
+            'template' => 'nullable|alpha_dash|max:255',
             'image' => 'image|max:2000',
-            'slug.*' => 'alpha_dash|max:255',
-            'title.*' => 'max:255',
-            'meta_keywords.*' => 'max:255',
-            'meta_description.*' => 'max:255',
+            'slug.*' => 'nullable|alpha_dash|max:255',
+            'title.*' => 'nullable|max:255',
+            'meta_keywords.*' => 'nullable|max:255',
+            'meta_description.*' => 'nullable|max:255',
         ];
     }
 }
