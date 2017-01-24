b69901fc89d74b1ba232be093de72d240c22ecad
Samuel De Backer <sdebacker@gmail.com>
2017-01-24 23:05:52 +0100

    @lang

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
     @include('core::admin._button-create', ['module' => 'pages'])
 
     <h1>
-        <span>__('Pages')</span>
+        <span>@lang('Pages')</span>
     </h1>
 
     <div class="btn-toolbar">
