ab42b1d9a86905f96b970b32acb8ded6384941b7
Samuel De Backer <sdebacker@gmail.com>
2017-02-12 22:01:11 +0100

    pages tree collapsed state saved in a cookie

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -18,7 +18,7 @@
 
     <div ui-tree="treeOptions">
         <ul ui-tree-nodes="" data-max-depth="3" ng-model="models" id="tree-root">
-            <li ng-repeat="model in models" ui-tree-node ng-include="'/views/partials/listItemPage.html'"></li>
+            <li ng-repeat="model in models" ui-tree-node collapsed="treeOptions.collapsed(this)" ng-include="'/views/partials/listItemPage.html'"></li>
         </ul>
     </div>
 
