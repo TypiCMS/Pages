629ae936b690faa695a22d4399b43b434f0eea2f
Samuel De Backer <sdebacker@gmail.com>
2017-05-24 21:42:35 +0200

    parameters

--- src/resources/views/admin/edit-section.blade.php
+++ src/resources/views/admin/edit-section.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 
-    <a class="btn-back" href="{{ route('admin::edit-page', $model->page_id) }}" title="{{ __('page_sections::global.Back') }}"><span class="text-muted fa fa-arrow-circle-left"></span><span class="sr-only">{{ __('page_sections::global.Back') }}</span></a>
+    <a class="btn-back" href="{{ route('admin::edit-page', $page->id) }}" title="{{ __('page_sections::global.Back') }}"><span class="text-muted fa fa-arrow-circle-left"></span><span class="sr-only">{{ __('page_sections::global.Back') }}</span></a>
 
     <h1 class="@if(!$model->present()->title)text-muted @endif">
         {{ $model->present()->title ?: __('Untitled') }}
     </h1>
 
-    {!! BootForm::open()->put()->action(route('admin::update-page_section', [$model->page_id, $model->id]))->multipart()->role('form') !!}
+    {!! BootForm::open()->put()->action(route('admin::update-page_section', [$page->id, $model->id]))->multipart()->role('form') !!}
     {!! BootForm::bind($model) !!}
         @include('pages::admin._form-section')
     {!! BootForm::close() !!}
