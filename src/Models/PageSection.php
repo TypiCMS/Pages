ca487d91e4a843e1c729d0fdc4525cbc49d6c768
Samuel De Backer <sdebacker@gmail.com>
2017-05-25 22:43:27 +0200

    SortableSectionTrait

--- src/Models/PageSection.php
+++ src/Models/PageSection.php
@@ -4,19 +4,19 @@ namespace TypiCMS\Modules\Pages\Models;
 
 use Laracasts\Presenter\PresentableTrait;
 use Spatie\EloquentSortable\Sortable;
-use Spatie\EloquentSortable\SortableTrait;
 use Spatie\Translatable\HasTranslations;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\Files\Models\File;
 use TypiCMS\Modules\History\Traits\Historable;
 use TypiCMS\Modules\Pages\Presenters\ModulePresenter;
+use TypiCMS\Modules\Pages\Traits\SortableSectionTrait;
 
-class PageSection extends Base implements Sortable
+class PageSection extends Base
 {
     use HasTranslations;
     use Historable;
     use PresentableTrait;
-    use SortableTrait;
+    use SortableSectionTrait;
 
     protected $presenter = ModulePresenter::class;
 
@@ -27,10 +27,6 @@ class PageSection extends Base implements Sortable
         'slug',
         'status',
         'body',
-    ];
-
-    public $sortable = [
-        'order_column_name' => 'position',
     ];
 
     protected $appends = ['image', 'title_translated', 'status_translated'];
