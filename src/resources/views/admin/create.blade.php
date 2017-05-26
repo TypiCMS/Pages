c12dccb56b24d8e87338efeabd15f38ca70602f7
Samuel De Backer <sdebacker@gmail.com>
2017-05-26 08:18:50 +0200

    translation

--- src/resources/views/admin/create.blade.php
+++ src/resources/views/admin/create.blade.php
@@ -13,6 +13,6 @@
         @include('pages::admin._form')
     {!! BootForm::close() !!}
 
-    <p class="alert alert-info">{{ __('Save the page then add sections.') }}</p>
+    <p class="alert alert-info">{{ __('Save this page first, then add sections.') }}</p>
 
 @endsection
