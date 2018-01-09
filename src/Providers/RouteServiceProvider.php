798bcac081e8b7fea6ace10a43b8ba67126788f9
Samuel De Backer <sdebacker@gmail.com>
2018-01-09 11:01:42 +0100

    Route methods chaining

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -24,12 +24,12 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        Route::group(['namespace' => $this->namespace], function (Router $router) {
+        Route::namespace($this->namespace)->group(function (Router $router) {
 
             /*
              * Admin routes
              */
-            $router->group(['middleware' => 'admin', 'prefix' => 'admin'], function (Router $router) {
+            $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
                 $router->get('pages', 'AdminController@index')->name('admin::index-pages')->middleware('can:see-all-pages');
                 $router->get('pages/create', 'AdminController@create')->name('admin::create-page')->middleware('can:create-page');
                 $router->get('pages/{page}/edit', 'AdminController@edit')->name('admin::edit-page')->middleware('can:update-page');
