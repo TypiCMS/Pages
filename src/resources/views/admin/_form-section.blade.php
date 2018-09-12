48baa0e2bb11b02a04112231ed6fd4ca9b771fe8
Samuel De Backer <sdebacker@gmail.com>
2018-09-13 00:44:46 +0200

    form-group around checkbox

--- src/resources/views/admin/_form-section.blade.php
+++ src/resources/views/admin/_form-section.blade.php
@@ -9,9 +9,9 @@
 {!! BootForm::hidden('page_id')->value($page->id) !!}
 {!! BootForm::hidden('position') !!}
 
-@include('files::admin._files-selector')
-
 @include('core::form._title-and-slug')
-{!! TranslatableBootForm::hidden('status')->value(0) !!}
-{!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+<div class="form-group">
+    {!! TranslatableBootForm::hidden('status')->value(0) !!}
+    {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+</div>
 {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor') !!}
