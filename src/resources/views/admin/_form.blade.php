ca2e7094197b36827105e6dfd4ae808609e523a9
Samuel De Backer <sdebacker@gmail.com>
2018-09-21 12:23:22 +0200

    position field in section list

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -77,6 +77,7 @@
                 <item-list-column-header name="checkbox"></item-list-column-header>
                 <item-list-column-header name="edit"></item-list-column-header>
                 <item-list-column-header name="status_translated" sortable :sort-array="sortArray" :label="$t('Status')"></item-list-column-header>
+                <item-list-column-header name="position" sortable :sort-array="sortArray" :label="$t('Position')"></item-list-column-header>
                 <item-list-column-header name="image" :label="$t('Image')"></item-list-column-header>
                 <item-list-column-header name="title_translated" sortable :sort-array="sortArray" :label="$t('Title')"></item-list-column-header>
             </template>
@@ -85,6 +86,7 @@
                 <td class="checkbox"><item-list-checkbox :model="model" :checked-models-prop="checkedModels" :loading="loading"></item-list-checkbox></td>
                 <td>@include('core::admin._button-edit', ['segment' => 'sections', 'module' => 'page_sections'])</td>
                 <td><item-list-status-button :model="model"></item-list-status-button></td>
+                <td><item-list-position-input :model="model"></item-list-position-input></td>
                 <td><img :src="model.thumb" alt=""></td>
                 <td>@{{ model.title_translated }}</td>
             </template>
