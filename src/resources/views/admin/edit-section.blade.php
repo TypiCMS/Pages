09329be9dfbaa6ecc2eff1478a5d6a4b40c20068
Samuel De Backer <sdebacker@gmail.com>
2018-09-28 17:31:25 +0200

    header markup

--- src/resources/views/admin/edit-section.blade.php
+++ src/resources/views/admin/edit-section.blade.php
@@ -4,11 +4,12 @@
 
 @section('content')
 
-    <a class="btn-back" href="{{ route('admin::edit-page', $page->id) }}" title="{{ __('Back to page') }}"><span class="text-muted fa fa-arrow-circle-left"></span><span class="sr-only">{{ __('Back to page') }}</span></a>
-
-    <h1 class="@if (!$model->present()->title)text-muted @endif">
-        {{ $model->present()->title ?: __('Untitled') }}
-    </h1>
+    <div class="header">
+        @include('core::admin._button-back', ['url' => route('admin::edit-page', $page)])
+        <h1 class="header-title @if (!$model->present()->title)text-muted @endif">
+            {{ $model->present()->title ?: __('Untitled') }}
+        </h1>
+    </div>
 
     {!! BootForm::open()->put()->action(route('admin::update-page_section', [$page->id, $model->id]))->multipart()->role('form') !!}
     {!! BootForm::bind($model) !!}
