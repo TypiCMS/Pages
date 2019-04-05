73f479b455e8134d75433152d5ba4f58f817b5e8
Samuel De Backer <sdebacker@gmail.com>
2019-04-05 14:38:33 +0200

    classes

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,9 +1,9 @@
-<li class="pages-item {{ Request::is($child->uri()) ? 'active' : '' }}" id="page_{{ $child->id }}">
-    <a class="pages-item-link" href="{{ url($child->uri()) }}">
+<li class="page-list-item {{ Request::is($child->uri()) ? 'active' : '' }}" id="page_{{ $child->id }}">
+    <a class="page-list-item-link" href="{{ url($child->uri()) }}">
         {{ $child->title }}
     </a>
     @if ($child->items)
-        <ul class="pages-item-children">
+        <ul class="page-list-item-children">
             @foreach ($child->items as $childPage)
                 @include('pages::public._list-item', ['child' => $childPage])
             @endforeach
