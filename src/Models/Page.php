d94814f58acb1b776d5c4297efccb05803dc0048
Samuel De Backer <sdebacker@gmail.com>
2019-09-02 16:31:23 +0200

    methods moved from repository

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -58,6 +58,24 @@ class Page extends Base
         return $uri ?: '/';
     }
 
+    public function getForRoutes()
+    {
+        $pages = $this->where('module', '!=', null)
+            ->with('files')
+            ->get();
+
+        return $pages;
+    }
+
+    public function allForSelect()
+    {
+        $pages = $this->get()
+            ->nest()
+            ->listsFlattened();
+
+        return ['' => ''] + $pages;
+    }
+
     public function getSubMenu()
     {
         $rootUriArray = explode('/', $this->uri);
