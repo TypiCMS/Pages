4d8e8c1f80a9f33a1926dcb1a7dcadf45d3fe175
Samuel De Backer <sdebacker@gmail.com>
2020-03-12 09:29:25 +0100

    Test StyleCI

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -18,12 +18,9 @@ class ModuleProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'typicms.pages'
-        );
-        $this->mergeConfigFrom(__DIR__.'/../config/config-sections.php', 'typicms.page_sections'
-        );
-        $this->mergeConfigFrom(__DIR__.'/../config/permissions.php', 'typicms.permissions'
-        );
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'typicms.pages');
+        $this->mergeConfigFrom(__DIR__.'/../config/config-sections.php', 'typicms.page_sections');
+        $this->mergeConfigFrom(__DIR__ . '/../config/permissions.php', 'typicms.permissions');
 
         $modules = $this->app['config']['typicms']['modules'];
         $this->app['config']->set('typicms.modules', array_merge(['pages' => []], $modules));
