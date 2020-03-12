1d57cdd2ddcd489ae06e028c65845fb1a8d529ff
Samuel De Backer <sdebacker@gmail.com>
2020-03-12 09:33:46 +0100

    Concat without space

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -20,7 +20,7 @@ class ModuleProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'typicms.pages');
         $this->mergeConfigFrom(__DIR__.'/../config/config-sections.php', 'typicms.page_sections');
-        $this->mergeConfigFrom(__DIR__ . '/../config/permissions.php', 'typicms.permissions');
+        $this->mergeConfigFrom(__DIR__.'/../config/permissions.php', 'typicms.permissions');
 
         $modules = $this->app['config']['typicms']['modules'];
         $this->app['config']->set('typicms.modules', array_merge(['pages' => []], $modules));
