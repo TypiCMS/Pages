92abdaed83c2fade5eeb7d854f329678dd2cc21d
Samuel De Backer <sdebacker@gmail.com>
2020-05-20 15:08:08 +0200

    og image size

--- src/resources/views/public/master.blade.php
+++ src/resources/views/public/master.blade.php
@@ -5,7 +5,7 @@
 @section('description', $page->meta_description)
 @section('keywords', $page->meta_keywords)
 @if ($page->image)
-@section('image', url($page->present()->image()))
+@section('image', $page->present()->image(1200, 630))
 @endif
 @section('bodyClass', 'body-page body-page-'.$page->id)
 
