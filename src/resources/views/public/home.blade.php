bff1db5ffa8c6bb639f491438163f89ca757944f
Samuel De Backer <sdebacker@gmail.com>
2020-09-17 15:43:13 +0200

    db. removed

--- src/resources/views/public/home.blade.php
+++ src/resources/views/public/home.blade.php
@@ -24,9 +24,9 @@
 {{--
     @if ($latestNews = News::published()->order()->take(3)->get() and $latestNews->count() > 0)
         <div class="news-list-container">
-            <h3 class="news-list-title"><a href="{{ Route::has($lang.'::index-news') ? route($lang.'::index-news') : '/' }}">@lang('db.Latest news')</a></h3>
+            <h3 class="news-list-title"><a href="{{ Route::has($lang.'::index-news') ? route($lang.'::index-news') : '/' }}">@lang('Latest news')</a></h3>
             @include('news::public._list', ['items' => $latestNews])
-            <a class="news-list-btn-more btn btn-light btn-sm" href="{{ Route::has($lang.'::index-news') ? route($lang.'::index-news') : '/' }}">@lang('db.All news')</a>
+            <a class="news-list-btn-more btn btn-light btn-sm" href="{{ Route::has($lang.'::index-news') ? route($lang.'::index-news') : '/' }}">@lang('All news')</a>
         </div>
     @endif
 --}}
@@ -34,9 +34,9 @@
 {{--
     @if ($upcomingEvents = Events::upcoming() and $upcomingEvents->count() > 0)
         <div class="event-list-container">
-            <h3 class="event-list-title"><a href="{{ Route::has($lang.'::index-events') ? route($lang.'::index-events') : '/' }}">@lang('db.Upcoming events')</a></h3>
+            <h3 class="event-list-title"><a href="{{ Route::has($lang.'::index-events') ? route($lang.'::index-events') : '/' }}">@lang('Upcoming events')</a></h3>
             @include('events::public._list', ['items' => $upcomingEvents])
-            <a class="event-list-btn-more btn btn-light btn-sm" href="{{ Route::has($lang.'::index-events') ? route($lang.'::index-events') : '/' }}">@lang('db.All events')</a>
+            <a class="event-list-btn-more btn btn-light btn-sm" href="{{ Route::has($lang.'::index-events') ? route($lang.'::index-events') : '/' }}">@lang('All events')</a>
         </div>
     @endif
 --}}
@@ -44,9 +44,9 @@
 {{--
     @if ($partners = Partners::published()->where('homepage', 1)->get() and $partners->count() > 0)
         <div class="partner-list-container">
-            <h3 class="partner-list-title"><a href="{{ Route::has($lang.'::index-partners') ? route($lang.'::index-partners') : '/' }}">@lang('db.Partners')</a></h3>
+            <h3 class="partner-list-title"><a href="{{ Route::has($lang.'::index-partners') ? route($lang.'::index-partners') : '/' }}">@lang('Partners')</a></h3>
             @include('partners::public._list', ['items' => $partners])
-            <a class="partner-list-btn-more btn btn-light btn-sm" href="{{ Route::has($lang.'::index-partners') ? route($lang.'::index-partners') : '/' }}">@lang('db.All partners')</a>
+            <a class="partner-list-btn-more btn btn-light btn-sm" href="{{ Route::has($lang.'::index-partners') ? route($lang.'::index-partners') : '/' }}">@lang('All partners')</a>
         </div>
     @endif
 --}}
