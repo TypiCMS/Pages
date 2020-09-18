89adb150853a15a8e9bf6a2491f14d09824a04d9
Samuel De Backer <sdebacker@gmail.com>
2020-09-18 18:25:32 +0200

    custom-select class

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -112,10 +112,10 @@
             {!! BootForm::hidden('redirect')->value(0) !!}
             {!! BootForm::checkbox(__('Redirect to first child'), 'redirect') !!}
         </div>
-        {!! BootForm::select(__('Module'), 'module', TypiCMS::getModulesForSelect())->disable($model->subpages->count() > 0)->helpBlock($model->subpages->count() ? __('A page containing subpages cannot be linked to a module') : '') !!}
-        {!! BootForm::select(__('Template'), 'template', TypiCMS::templates())->helpBlock(TypiCMS::getTemplateDir()) !!}
+        {!! BootForm::select(__('Module'), 'module', TypiCMS::getModulesForSelect())->disable($model->subpages->count() > 0)->helpBlock($model->subpages->count() ? __('A page containing subpages cannot be linked to a module') : '')->addClass('custom-select') !!}
+        {!! BootForm::select(__('Template'), 'template', TypiCMS::templates())->helpBlock(TypiCMS::getTemplateDir())->addClass('custom-select') !!}
         @if (!$model->id)
-        {!! BootForm::select(__('Add to menu'), 'add_to_menu', ['' => ''] + Menus::all()->pluck('name', 'id')->all(), null, ['class' => 'form-control']) !!}
+        {!! BootForm::select(__('Add to menu'), 'add_to_menu', ['' => ''] + Menus::all()->pluck('name', 'id')->all(), null, ['class' => 'form-control'])->addClass('custom-select') !!}
         @endif
         {!! BootForm::textarea(__('Css'), 'css') !!}
         {!! BootForm::textarea(__('Js'), 'js') !!}
