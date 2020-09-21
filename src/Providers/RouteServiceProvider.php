398da207760bdeef95db1ea3cf181b4c7c9b895a
Samuel De Backer <sdebacker@gmail.com>
2020-09-21 20:51:19 +0200

    Update RouteServiceProvider.php

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -14,9 +14,6 @@ use TypiCMS\Modules\Pages\Http\Controllers\SectionsApiController;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * Define the routes for the application.
-     */
     public function map()
     {
         /*
