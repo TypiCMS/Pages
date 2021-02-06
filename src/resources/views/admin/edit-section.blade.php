ec17e91833a98894efcdb965dd307f128af3b09b
Samuel De Backer <sdebacker@gmail.com>
2021-02-06 21:08:18 +0100

    Revert "Update edit-section.blade.php"
    This reverts commit 6ee58607b7d560a7fa6b32a2620aacc1cd9bfc6a.

--- src/resources/views/admin/edit-section.blade.php
+++ src/resources/views/admin/edit-section.blade.php
@@ -6,7 +6,7 @@
 
     <div class="header">
         @include('core::admin._button-back', ['url' => route('admin::edit-page', $page)])
-        <h1 class="header-title @empty($model->title)text-muted @endempty">
+        <h1 class="header-title @if (!$model->present()->title)text-muted @endif">
             {{ $model->present()->title ?: __('Untitled') }}
         </h1>
     </div>
